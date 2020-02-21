c48680bca7d417199222e2ee7351d60dabca191d
Krissada Jindanupajit <krissada.jindanupajit@smoothstack.com>
2020-02-20 23:25:09 -0500

    Migrate

---
+++ src/test/java/com/smoothstack/lms/lmsorchestrator/LmsOrchestratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.smoothstack.lms.lmsorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
